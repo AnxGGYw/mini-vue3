5865f751f312a663895a85288793503cbc952763
cggcbb <449567533@qq.com>
2023-01-06 17:21:57 +0800

    feat: effect support stop, onStop

--- src/reactivity/__test__/effect.spec.ts
+++ src/reactivity/__test__/effect.spec.ts
@@ -1,4 +1,4 @@
-import { effect } from '../effect'
+import { effect, stop } from '../effect'
 import { reactive } from '../reactive'
 
 describe('effect', () => {
@@ -7,27 +7,27 @@ describe('effect', () => {
       age: 22
     })
 
-    let effectAge
+    let dummy
     effect(() => {
-      effectAge = user.age
+      dummy = user.age
     })
-    expect(effectAge).toBe(22)
+    expect(dummy).toBe(22)
 
     // update
     user.age++
-    expect(effectAge).toBe(23)
+    expect(dummy).toBe(23)
   })
 
   it('should return runner when called effect', () => {
-    let age = 22
+    let dummy = 22
     const runner: any = effect(() => {
-      age++
+      dummy++
       return 'anXin'
     })
-    expect(age).toBe(23)
+    expect(dummy).toBe(23)
     // 执行runner
     const r = runner()
-    expect(age).toBe(24)
+    expect(dummy).toBe(24)
     expect(r).toBe('anXin')
   })
 
@@ -36,7 +36,7 @@ describe('effect', () => {
     // 2. effect第一次执行的时候，还会执行第一个参数的fn
     // 3. 当响应式对象 set update 的时候，不执行第一个参数的fn，而是执行scheduler
     // 4. 执行 effect 返回值 runner 的时候，会再次执行第一个参数的fn
-    let age
+    let dummy
     let run
 
     const scheduler = jest.fn(() => {
@@ -47,21 +47,61 @@ describe('effect', () => {
     })
     const runner = effect(
       () => {
-        age = user.age
+        dummy = user.age
       },
       { scheduler }
     )
     // 第一次scheduler不会被执行
     expect(scheduler).not.toHaveBeenCalled()
     // effect第一个参数fn执行
-    expect(age).toBe(22)
+    expect(dummy).toBe(22)
     user.age++
     // 响应式数据变化时，只会执行scheduler, 不会执行effect第一个参数fn
     expect(scheduler).toHaveBeenCalledTimes(1)
-    expect(age).toBe(22)
+    expect(dummy).toBe(22)
     // 执行effect返回值runner时，只会执行effect第一个参数fn，不会执行scheduler
     run()
-    expect(age).toBe(23)
+    expect(dummy).toBe(23)
     expect(scheduler).toHaveBeenCalledTimes(1)
+  })
+
+  it('stop', () => {
+    let dummy
+    const user = reactive({
+      age: 22
+    })
+    const runner = effect(() => {
+      dummy = user.age
+      return 'anXin'
+    })
+    user.age = 23
+    expect(dummy).toBe(23)
+    stop(runner)
+    user.age = 24
+    expect(dummy).toBe(23)
+    const r = runner()
+    expect(r).toBe('anXin')
+    expect(dummy).toBe(24)
+  })
+
+  it('onStop', () => {
+    let dummy
+    const user = reactive({
+      age: 22
+    })
+    const onStop = jest.fn()
+    const runner = effect(
+      () => {
+        dummy = user.age
+      },
+      {
+        onStop
+      }
+    )
+    expect(dummy).toBe(22)
+    user.age++
+    stop(runner)
+    expect(onStop).toBeCalledTimes(1)
+    expect(dummy).toBe(23)
   })
 })
--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,26 +1,56 @@
+import { extend } from '../shared'
+
 let activeEffect: any
 let targetMap = new WeakMap()
 
 interface effectOptions {
   scheduler?: Function
+  onStop?: Function
 }
 
 class ReactiveEffect {
   private fn: Function
-  constructor(fn: Function, public scheduler?: Function) {
+  deps = []
+  active = true
+  onStop: Function
+  scheduler?: Function
+  constructor(fn: Function) {
     this.fn = fn
   }
   run() {
     activeEffect = this
     return this.fn()
   }
+  stop() {
+    if (this.active) {
+      if (this.onStop) {
+        this.onStop()
+      }
+      clearUpEffect(this)
+      this.active = false
+    }
+  }
+}
+
+// 清空当前effect
+function clearUpEffect(effect: any) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect)
+  })
 }
 
 export function effect(fn: Function, options: effectOptions = {}) {
-  const instance = new ReactiveEffect(fn, options.scheduler)
-  instance.run()
-  // return runner
-  return instance.run.bind(instance)
+  // fn
+  const _effect = new ReactiveEffect(fn)
+
+  // 合并options
+  extend(_effect, options)
+
+  _effect.run()
+
+  const runner = _effect.run.bind(_effect)
+  runner.effect = _effect
+  return runner
 }
 
 // 收集依赖
@@ -35,7 +65,12 @@ export function track(target: any, key: string | symbol) {
     dep = new Set()
     depsMap.set(key, dep)
   }
+
+  if (!activeEffect) return
+
   dep.add(activeEffect)
+  // effect反向收集dep，stop方法才能知道
+  activeEffect.deps.push(dep)
 }
 
 // 派发更新，触发依赖
@@ -52,4 +87,8 @@ export function trigger(target: any, key: string | symbol) {
       }
     }
   }
+}
+
+export function stop(runner: any) {
+  runner.effect.stop()
 }
